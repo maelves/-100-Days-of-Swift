80bea88a9c1939e2c8b0325543f1a301039eda47
maruseanicoara <ioana.maru00@gmail.com>
2021-06-15 23:40:21 +0300

    Day 5

---
+++ Days 1-12/Learning (Day 5).playground/Contents.swift
@@ -0,0 +1,137 @@
+import UIKit
+
+// Day 5 \\
+// Writing functions
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+
+printHelp()
+
+// Accepting parameters
+func square(number: Int) {
+    print(number * number)
+}
+
+square(number: 5)
+
+// Returning values
+func square2(number: Int) -> Int {
+    return number * number
+}
+
+let result = square2(number: 8)
+print(result)
+
+// Parameter labels
+func sayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(to: "Mark")
+
+// Omitting parameter labels
+func greet(_ person: String) {
+    print("Hello, \(person)!")
+}
+
+greet("Lowe")
+
+// Default parameters
+func greetNicely(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+
+greetNicely("Adam")
+greetNicely("Nichole", nicely: false)
+
+// Variadic functions
+func squareVariadic(numbers: Int...) {
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+
+squareVariadic(numbers: 1, 2, 3, 4, 5)
+
+// Writing throwing functions
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+// Running throwing functions
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+// inout parameters
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+
+var myNum = 10
+doubleInPlace(number: &myNum)
+
+
+// Day 6 \\
+// Creating basic closures
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+// Accepting parameters in a closure
+let driving2 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+driving2("New York")
+
+// Returning values from a closure
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("Paris")
+print(message)
+
+// Closures as parameters
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
+// Trailing closure syntax
+travel(){
+    print("I am going places with my car.")
+}
+
+travel{
+    print("I am going places with my cars.")
+}
